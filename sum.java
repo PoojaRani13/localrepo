f9b12b366423f80463279a8eed403c431c180b21
PoojaRani13 <puja2016pooja2016@gmail.com>
2023-11-30 14:14:55 +0530

    Add Initial Files

---
+++ sum.java
@@ -0,0 +1,28 @@
+package localRepo;
+
+
+import java.util.Scanner;
+public class sum {
+    
+    public static void main(String[] args) {
+        int n,sum=0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the numners of elements in the array");
+        n=s.nextInt();
+        int []a=new int[n];
+        System.out.println("Enter the elements of the array");
+
+        for(int i=0;i<n;i++){
+
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]%2==0){
+                sum=sum+a[i];
+            }
+
+        }
+        System.err.println("sum of even numbers:"+sum);
+
+    }
+}
